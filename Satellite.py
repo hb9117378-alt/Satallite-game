9f761fd95f97ccbb3a9805e3edfc000ca0bd969b
hb9117378-alt <hb9117378@gmail.com>
2026-04-22 17:58:09 +0200

    Add files via upload

---
+++ Satellite.py
@@ -0,0 +1,14 @@
+import pgzrun
+from random import randint
+from time  import time
+
+WIDTH=800
+HEIGHT=800
+
+Satellites=[]
+Lines=[]
+Next_satellites=0
+Start=0
+End=0
+Total=0
+Number_of_satallites=10
